83158f22430619f4dfe38bbfad728d730f06da45
tapia1013 <manbearpig1013@gmail.com>
2022-04-23 15:30:00 -0700

    updated our app with shop page, collection preview component and collection item component with styles

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
+
 
 import './collection-preview.styles.scss';
 
@@ -9,10 +11,8 @@ const CollectionPreview = ({ title, items }) => (
       {
         items
           .filter((item, idx) => idx < 4)
-          .map((item) => (
-            <div key={item.id}>
-              {item.name}
-            </div>
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
           ))
       }
     </div>
