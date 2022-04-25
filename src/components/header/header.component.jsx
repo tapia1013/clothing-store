b5cd88b43428041adea9b79928d9f19ba834557d
tapia1013 <manbearpig1013@gmail.com>
2022-04-25 12:47:36 -0700

    cart component icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 
 import { auth } from '../../firebase/firebase.utils'
-
+import CartIcon from '../cart-icon/cart-icon.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
@@ -35,6 +35,7 @@ const Header = ({ currentUser }) => (
             SIGN IN
           </Link>)
       }
+      <CartIcon />
     </div>
   </div>
 );
